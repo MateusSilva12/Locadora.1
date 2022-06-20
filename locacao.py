2c4d0c78f2bd5bd07aca096e62ab579dc4ae0966
vitoria <sousa162019@gmail.com>
2022-06-20 14:49:31 -0300

    operacao

---
+++ locacao.py
@@ -0,0 +1,7 @@
+from operacao import Operacao
+class Locacao:
+    def __init__(self,periodo):
+        self._periodo=int()
+    def setPeriodo(self,periodo):
+        self._periodo=int()
+    def getPeriodo(self):
--- reserva.py
+++ reserva.py
@@ -1,3 +1,9 @@
 from operacao import Operacao
-
+class Reserva:
+   def __init__(self,prioridade):
+       self._prioridade=int()
+   def setPrioridade(self,prioridade):
+       self._prioridade=int()
+   def getPrioridades(self,prioridades):
+       return self._prioridade
 
